b8a1ad484630216c0d23e4546b68320e2679c100
Kirbyret <2020030313@upsin.edu.mx>
2022-10-20 00:19:03 -0600

    validacion de caja de texto

--- js/appP4.js
+++ js/appP4.js
@@ -6,6 +6,7 @@ function valorProm(calificaciones){
     }
     return suma / longitudCalif;
 }
+
 let calificaciones = [6, 10, 6, 9, 10, 8, 9, 10, 10, 6, 5, 8, 9, 10, 6, 7, 10, 7, 4, 8];
 console.log("El promedio de las calificaciones es de: " +valoProm(calificaciones));
 
@@ -20,10 +21,15 @@ function valorPares(numeros){
     return contador;
 }
 // El arreglo de 20 posiciones para los numeros
-let numeros = [54 , 61 , 71 , 21 , 52 , 34 , 48 , 49 , 27 , 68 , 73 , 32 , 96 , 89 , 70 , 47 , 24 , 5 , 7 , 45];
+let numeros = [51 , 40 , 2 , 4 , 54 , 23 , 75 , 10 , 28 , 29 , 62 , 24 , 89 , 24 , 54 , 25 , 87 , 6 , 8 , 74];
 console.log("La cantidad de números pares es de: " + valorPares(numeros));
 
-
-// Hacer Commit 'Generación de numeros aleatorios
 // Hacer Commit 'Validación de caja de texto (limite) REQUERIDO Y NUMERICO
+function validar(){
+    validarLimite = document.querySelector('#limite').value;
+
+    if(validarLimite == 0){
+        alert('Por favor, Capture un valor');
+    }
+}
 // Hacer Commit con la listanumeros ORDENADOS ASCENDENTE
